c5ef9910a702a4ac6b04ba6144a207fe1c209d80
Said G <saidgurrola@gmail.com>
2025-04-05 13:24:05 -0600

    Fixture fixed to run tests

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,17 +6,22 @@ from utils.config import Config
 class TestLogin:
 
     @pytest.fixture(scope="class", autouse=True)
-    def setup_class(self):
+    def setup_class(self, request):
+        request.cls.driver = webdriver.Chrome()  # You can change this to your preferred WebDriver
+        request.cls.driver.get(Config.URL)
+        request.cls.login_page = LoginPage(request.cls.driver)
+        yield
+        request.cls.driver.quit()
         self.driver = webdriver.Chrome()  # You can change this to your preferred WebDriver
         self.driver.get(Config.URL)
         self.login_page = LoginPage(self.driver)
         yield
         self.driver.quit()
 
-    def test_login_success(self, setup):
+    def test_login_success(self):
         self.login_page.login(Config.VALID_USERNAME, Config.VALID_PASSWORD)
         assert self.login_page.is_login_successful() == True
 
-    def test_login_failure(self, setup):
+    def test_login_failure(self):
         self.login_page.login(Config.INVALID_USERNAME, Config.INVALID_PASSWORD)
         assert self.login_page.is_login_successful() == False
--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,6 @@
 class Config:
     URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    valid_username = "Admin"
-    valid_password = "admin123"
-    invalid_username = "InvalidUser"
-    invalid_password = "InvalidPass"
+    VALID_USERNAME = "Admin"
+    VALID_PASSWORD = "admin123"
+    INVALID_USERNAME = "InvalidUser"
+    INVALID_PASSWORD = "InvalidPass"
