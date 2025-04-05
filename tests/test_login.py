9b2b5c8aac4e0b58df98e7e8db1ee48e2b657c8d
Said G <saidgurrola@gmail.com>
2025-04-05 14:41:49 -0600

    Addes time module to pass success test, needs to be modified

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 import pytest
 from pages.login_page import LoginPage
 from utils.config import Config
+import time
 
 class TestLogin:
 
@@ -10,18 +11,16 @@ class TestLogin:
         request.cls.driver = webdriver.Chrome()  # You can change this to your preferred WebDriver
         request.cls.driver.get(Config.URL)
         request.cls.login_page = LoginPage(request.cls.driver)
+        time.sleep(5)  # Wait for the page to load    
         yield
         request.cls.driver.quit()
-        self.driver = webdriver.Chrome()  # You can change this to your preferred WebDriver
-        self.driver.get(Config.URL)
-        self.login_page = LoginPage(self.driver)
-        yield
-        self.driver.quit()
 
     def test_login_success(self):
         self.login_page.login(Config.VALID_USERNAME, Config.VALID_PASSWORD)
+        time.sleep(5)
         assert self.login_page.is_login_successful() == True
 
     def test_login_failure(self):
         self.login_page.login(Config.INVALID_USERNAME, Config.INVALID_PASSWORD)
+        time.sleep(5)
         assert self.login_page.is_login_successful() == False
